d3b45923e9fbff86f8c76e72a79bc56c339acf55
everetteallen <oitadmin@itdhcp10136.unity.ncsu.edu>
2016-03-29 10:03:05 -0400

    added variable for server

--- 62nuke.sh
+++ 62nuke.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 ## Netboots at 10.6.8 system to 10.7+ nbi named nuke.nbi
 ##
-bless --netboot  --booter tftp://152.1.224.46/NetBoot/NetBootSP0/nuke.nbi/i386/booter --kernel tftp://152.1.224.46/NetBoot/NetBootSP0/nuke.nbi/i386/x86_64/kernelcache --options "rp=nfs:152.1.224.46:/private/tftpboot/NetBoot/NetBootSP0:nuke.nbi/NetBoot.dmg"
+nbserver=""
+bless --netboot  --booter tftp://$nbserver/NetBoot/NetBootSP0/nuke.nbi/i386/booter --kernel tftp://$nbserver/NetBoot/NetBootSP0/nuke.nbi/i386/x86_64/kernelcache --options "rp=nfs:$nbserver:/private/tftpboot/NetBoot/NetBootSP0:nuke.nbi/NetBoot.dmg"
 nvram "$( nvram efi-boot-file | sed -E 's/^efi-boot-file[[:space:]]*/efi-boot-kernelcache=/' )"
 nvram -d efi-boot-file
 nvram boot-args="-v"
